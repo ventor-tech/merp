b2fcd7b59a633a6e838cdd89340059445f3508a5
Vladislav Kakovko <vladislav.k@ventor.tech>
2019-12-30 12:04:36 +0300

    [ADD]ONSHORE-8: Migrate module to 13.0

--- product_multiple_barcodes/__manifest__.py
+++ product_multiple_barcodes/__manifest__.py
@@ -3,13 +3,14 @@
 
 {
     'name': 'Product Multiple Barcodes',
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     'author': 'VentorTech',
     'website': 'https://ventor.tech/',
     'license': 'LGPL-3',
     'installable': True,
     'images': ['static/description/main_banner.png'],
-    'summary': 'Allows adding additional barcodes to your product.product and product.template models and search by these barcodes',
+    'summary': 'Allows adding additional barcodes to your product.product '
+               'and product.template models and search by these barcodes',
     'depends': [
         'product',
     ],
--- product_multiple_barcodes/models/product_template.py
+++ product_multiple_barcodes/models/product_template.py
@@ -1,10 +1,21 @@
 # Copyright 2019 VentorTech OU
 # Part of Ventor modules. See LICENSE file for full copyright and licensing details.
 
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    barcode_ids = fields.One2many(related='product_variant_ids.barcode_ids', readonly=False)
+    barcode_ids = fields.One2many(
+        related='product_variant_ids.barcode_ids',
+        readonly=False
+    )
+
+    @api.model
+    def create(self, vals):
+        if vals.get('barcode_ids'):
+            ctx = self.env.context.copy()
+            ctx['barcode'] = vals['barcode_ids']
+            self.env.context = ctx
+        return super(ProductTemplate, self).create(vals)
